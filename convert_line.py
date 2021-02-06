8e3486ae52b21602784912c8e8af3d089301b780
Alexander Yang <a.yang7005@gmail.com>
2021-02-06 09:38:24 -0500

    Added images for testing

---
+++ convert_line.py
@@ -0,0 +1,56 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv.imread('images/court4.png',0)
+img2 = cv.imread('images/court2.png', 0)
+#img = cv.medianBlur(img,5)
+# ret,th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+# th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,\
+#             cv.THRESH_BINARY,11,2)
+# th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
+#             cv.THRESH_BINARY_INV,11,15)
+# titles = ['Original Image', 'Global Thresholding (v = 127)',
+#             'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+# images = [img, th1, th2, th3]
+# for i in range(4):
+#     plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+#     plt.title(titles[i])
+#     plt.xticks([]),plt.yticks([])
+# plt.show()
+
+#cv.imshow('threshold.png', th3)
+
+#img = th3
+cannyThres1 = 200
+cannyThres2 = 200
+edges = cv.Canny(img,cannyThres1,cannyThres2)
+edges2 = cv.Canny(img, cannyThres1, cannyThres2)
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+#plt.show()
+
+
+cv.imshow("edges analysis", edges)
+
+rho = 1  # distance resolution in pixels of the Hough grid
+theta = np.pi / 180  # angular resolution in radians of the Hough grid
+threshold = 30  # minimum number of votes (intersections in Hough grid cell)
+min_line_length = 15  # minimum number of pixels making up a line
+max_line_gap = 10  # maximum gap in pixels between connectable line segments
+line_image = np.copy(img) * 0  # creating a blank to draw lines on
+
+# Run Hough on edge detected image
+# Output "lines" is an array containing endpoints of detected line segments
+lines = cv.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+                    min_line_length, max_line_gap)
+
+for line in lines:
+    for x1,y1,x2,y2 in line:
+        cv.line(line_image,(x1,y1),(x2,y2),(255,0,0), 1)
+
+lines_edges = cv.addWeighted(img, 0.8, line_image, 1, 0)
+
+cv.imshow("lines analysis", line_image)
+
+cv.waitKey()
