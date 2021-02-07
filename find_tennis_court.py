f50bc857d2232105048f293ffae1decbbe551d59
Alexander Yang <a.yang7005@gmail.com>
2021-02-06 19:13:33 -0500

    Combined two ML openCV algorithms

--- find_tennis_court.py
+++ find_tennis_court.py
@@ -4,7 +4,52 @@ import cv2 as cv
 import numpy as np
 from utilities import *
 
-def scan_courts_in_image(image_filename):
+def find_tennis_court_template_matching(image):
+
+    template1 = cv.imread('images/court1.png')
+    template2 = cv.imread('images/court2.png')
+    template3 = cv.imread('images/court3.png')
+    template4 = cv.imread('images/court4.png')
+    temp1 = cv.cvtColor(template1, cv.COLOR_BGR2GRAY)
+    temp2 = cv.cvtColor(template2, cv.COLOR_BGR2GRAY)
+    temp3 = cv.cvtColor(template3, cv.COLOR_BGR2GRAY)
+    temp4 = cv.cvtColor(template4, cv.COLOR_BGR2GRAY)
+
+    template = [temp1, temp2, temp3, temp4]
+
+    imagepath = ".\images\sateliteimage*.png"
+    images = glob.glob(imagepath)
+
+
+    img_rgb = cv.imread(image)
+    img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+
+    name_parse = image[:-4].split("_")
+    centerXCord = float(name_parse[1])
+    centerYCord = float(name_parse[2])
+
+    found_court = False
+    for temp in template:
+
+        w, h = temp1.shape[::-1]
+        res = cv.matchTemplate(img_gray, temp, cv.TM_CCOEFF_NORMED)
+        threshold = 0.9
+        loc = np.where(res >= threshold)
+        for pt in zip(*loc[::-1]):
+            found_court = True
+            cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255),
+                         2)
+
+    if found_court:
+        labeled_file_name = "./labeled_images/" + str(centerXCord) + "_" + str(
+            centerYCord) + "_" + "labeled.png"
+        cv.imwrite(labeled_file_name, img_rgb)\
+
+        return centerXCord, centerYCord, pt[0], pt[1]
+
+    return None
+
+def find_tennis_court_hough_analysis(image_filename):
 
     name_parse = image_filename[:-4].split("_")
     centerXCord = float(name_parse[1])
@@ -138,18 +183,28 @@ def find_tennis_court(list_of_images=None):
     imagepath = ".\images\sateliteimage*.png"
     images = glob.glob(imagepath)
 
-    final_result_list = []
+    result_list = []
     for image in images:
 
-        result = scan_courts_in_image(image)
+        print("Processing " + image)
+
+        result = find_tennis_court_hough_analysis(image)
 
         if result is not None:
-            final_result_list.append(result)
+            result_list.append(result)
+            continue
 
-    with_offsets = list(map(apply_offset_to_coordinates, final_result_list))
+        result = find_tennis_court_template_matching(image)
 
-    for i in range(0, len(with_offsets)):
-        with_offsets[i] = str(with_offsets[i])[1:-1].replace(" ", "")
+        if result is not None:
+            result_list.append(result)
+
+    with_offsets = list(map(apply_offset_to_coordinates, result_list))
+
+    final_dict_list = []
+
+    for lat, lng in with_offsets:
+        final_dict_list.append({"lat": lat, "lng": lng})
 
     return with_offsets
 
